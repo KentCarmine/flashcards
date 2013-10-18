f67723b4848ebfdc64b4ff3cf45c7016de3328c3
kotentie <jakekoten@gmail.com>
2013-10-18 13:27:34 -0500

    Create flash_presenter.rb

---
+++ flash_presenter.rb
@@ -0,0 +1,36 @@
+require "./flash_stack.rb"
+require "./flashcard.rb"
+
+
+
+
+class FlashPresenter
+  def display_card(card)
+    puts
+    puts card.definition
+    puts
+  end
+
+  def user_guess
+    puts "Guess the answer:"
+    puts
+    gets.chomp
+  end
+
+  def display_t_f(true_false)
+    puts
+    if true_false
+      puts "CORRECT"
+    else 
+      put "THAT'S FALSE LITTLE YO"
+    end
+    puts
+  end 
+end
+
+
+m = FlashPresenter.new
+m.display_card( FlashCard.new(:definition => "is it or not", :term => "badass"))
+
+m.user_guess
+m.display_t_f(true)

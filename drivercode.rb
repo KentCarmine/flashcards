df0753232e23eda9997fd35ce5f66a344c4f2037
Apprentice <apprentice@dbc-chicago031.local>
2013-10-18 11:27:58 -0500

    Added pseudocode in drivercode.rb

---
+++ drivercode.rb
@@ -0,0 +1,13 @@
+
+# Create FlashStack object
+#   Load from the file
+#   Populate FlashCard objects with file contents
+# 
+# Remove first flashcard from stack
+#   print definition on that card
+#   check that term of that card == "alias"
+#     if it does
+#       print WIN
+#     otherwise
+#       print FAIL
+
